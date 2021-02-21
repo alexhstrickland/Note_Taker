784ba62b8cd630c5bd2aff29c52b228b6998a26b
alexhstrickland <alexhstrickland@gmail.com>
2021-02-21 00:03:07 -0500

    npm package uuid is not assigning id to each note

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { Server } = require('http');
 const path = require('path');
 const fs = require('fs');
-
+const { v4: uuidv4 } = require('uuid');
 
 
 
@@ -21,11 +21,13 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')));
+app.get('/api/notes/:id', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')));
 
 
 app.post('/api/notes', (req, res) => {
     const newSavedNote = req.body;
     console.log(newSavedNote);
+    newSavedNote.id = uuidv4();
     const notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
     console.log(notes);
     notes.push(newSavedNote);
@@ -36,6 +38,10 @@ app.post('/api/notes', (req, res) => {
     return res.json({});
 })
 
-// app.delete()
+app.delete('/api/notes/:id', (req,res) => {
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
+    let it = notes.findIndex(newSavedNote => newSavedNote.id === 'uuid')
+    console.log(`delete ${req.params.id}`);
+})
 
 app.listen(PORT, () => console.log(`App listening on ${PORT}`));
