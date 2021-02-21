0e3373fedb50022e906389049e465ad3d8654b10
alexhstrickland <alexhstrickland@gmail.com>
2021-02-20 22:51:46 -0500

    can now add note to api and page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const { Server } = require('http');
 const path = require('path');
-const db = require('./db/db.json');
+const fs = require('fs');
 
 
 // Sets up the Express App
@@ -18,11 +18,23 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-app.get('/api/notes', (req, res) => res.json(db));
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')));
+
 
 app.post('/api/notes', (req, res) => {
     const newSavedNote = req.body;
     console.log(newSavedNote);
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
+    console.log(notes);
+    notes.push(newSavedNote);
+    console.log(notes);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), function(err, result) {
+        if(err) console.log('error', err)
+    });
+    return res.json({});
+
 })
+
+app.delete()
 
 app.listen(PORT, () => console.log(`App listening on ${PORT}`));
